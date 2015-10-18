16e292b223ee2d4e8a0d9831b1f321579487c069
Scott Bellware <sbellware@gmail.com>
2015-10-18 10:01:32 -0500

    Test logging is removed

--- test/spec/serialize_error_data.rb
+++ test/spec/serialize_error_data.rb
@@ -7,9 +7,6 @@ describe "Error Data Serialization" do
     error_data = RaygunClient::Controls::ErrorData.example
     json_text = RaygunClient::ErrorData::Serialize.(error_data)
 
-    __logger.focus compare_json_text
-    __logger.focus json_text
-
     assert(json_text == compare_json_text)
   end
 end
