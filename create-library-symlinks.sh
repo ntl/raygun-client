ba5df8dcda2c8c66741ddc76b3a23e1784bbac27
Scott Bellware <sbellware@gmail.com>
2015-10-27 14:08:51 -0500

    Symlink script error is corrected

--- create-library-symlinks.sh
+++ create-library-symlinks.sh
@@ -1,25 +1,33 @@
 set -e
 
+if [ -z ${LIBRARIES_DIR+x} ]; then
+  echo "LIBRARIES_DIR must be set to the libraries directory path... exiting"
+  exit 1
+fi
+
+if [ ! -d "$LIBRARIES_DIR" ]; then
+  echo "$LIBRARIES_DIR does not exit... exiting"
+  exit 1
+fi
+
 function symlink-library {
-  if [ -z ${LIBRARIES_DIR+x} ]; then
-    echo "LIBRARIES_DIR must be set to the libraries directory path"
-  else
-    name=$1
+  name=$1
 
-    echo
-    echo "Symlinking $name"
-    echo "- - -"
+  echo
+  echo "Symlinking $name"
+  echo "- - -"
 
-    for entry in $LIBRARIES_DIR/$name*; do
+  for entry in $LIBRARIES_DIR/$name*; do
+    if [ -e "$entry" ]; then
       echo "- removing symlink: $entry"
       rm $entry
-    done
+    fi
+  done
 
-    echo "- creating symlinks"
-    ln -s $(PWD)/lib/$name* $LIBRARIES_DIR/
+  echo "- creating symlinks"
+  ln -s $(PWD)/lib/$name* $LIBRARIES_DIR/
 
-    echo "- - -"
-    echo "($name done)"
-    echo
-  fi
+  echo "- - -"
+  echo "($name done)"
+  echo
 }
