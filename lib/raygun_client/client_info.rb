2130a078aac96d425e7db462ff5ddaadfa6a18a4
Nathan Ladd <nathanladd@gmail.com>
2020-07-13 17:30:04 -0500

    Client is updated

--- lib/raygun_client/client_info.rb
+++ lib/raygun_client/client_info.rb
@@ -1,7 +1,7 @@
 module RaygunClient
   module ClientInfo
     def self.name
-      'Obsidian Raygun Client'
+      'Raygun Client'
     end
 
     def self.version
@@ -9,7 +9,7 @@ module RaygunClient
     end
 
     def self.url
-      'https://github.com/obsidian-btc/raygun-client'
+      'https://github.com/ntl/raygun-client'
     end
   end
 end
